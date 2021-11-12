a3894d2ee767cc03124fff716587a4a593061a41
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2021-11-13 00:52:31 +0900

    Unify methods (size to length)

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
+# ! /usr/bin/env ruby
+
 require 'fileutils'
 
 METACHARACTER = '*'
-NUMBER_OF_FILES = Dir[METACHARACTER].size
+NUMBER_OF_FILES = Dir[METACHARACTER].length
 NUMBER_OF_COLUMNS = 3
 NUMBER_OF_ROWS = (NUMBER_OF_FILES.to_f / NUMBER_OF_COLUMNS).ceil
 
-# ファイル/ディレクトリ名の中で一番文字数が多いものの文字数を取得する
-def calc_character_count
+# ファイル/ディレクトリ名の中で一番文字数が多いものの文字数を取得するcd 05
+def max_character_count
   character_count = 0
   (0...NUMBER_OF_FILES).each do |index|
     character_count = Dir[METACHARACTER][index].length\
@@ -22,7 +24,7 @@ def filename(column, row)
 end
 
 def add_space(column, row)
-  ' ' * (calc_character_count + 3 - filename(column, row).length)
+  ' ' * (max_character_count + 3 - filename(column, row).length)
 end
 
 def main
