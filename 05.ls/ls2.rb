a9819b5c5cd5af181dee0f0964c2aeb27ad16d40
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2021-12-18 15:21:59 +0900

    fixed a variable:name

--- 05.ls/ls2.rb
+++ 05.ls/ls2.rb
@@ -6,7 +6,6 @@ require 'etc'
 require 'date'
 
 NUMBER_OF_COLUMNS = 1
-FILENAME_WIDTH = 25
 NLINK_WIDTH = 2
 DETAIL_WIDTH = 5
 TIMESTAMP_WIDTH = 15
@@ -26,27 +25,15 @@ end
 
 def main
   files = take_files
-  print_total(files)
-  (0...number_of_files(files)).each do |row|
+  print_total(files) if option[:line]
+  (0...number_of_rows(files)).each do |row|
     (0...NUMBER_OF_COLUMNS).each do |column|
-      file_detail(row, column, files) if option[:line]
-      print_filename(row, column, files)
+      name = files[column * number_of_rows(files) + row]
+      print_file_detail(name) if option[:line]
+      print name
     end
     print "\n"
   end
-end
-
-def number_of_rows(files)
-  (number_of_files(files).to_f / NUMBER_OF_COLUMNS).ceil
-end
-
-def number_of_files(files)
-  files.length
-end
-
-def print_filename(row, column, files)
-  name = files[column * number_of_rows(files) + row]
-  print name.ljust(FILENAME_WIDTH) if name
 end
 
 def print_total(files)
@@ -57,8 +44,17 @@ def print_total(files)
   puts "total #{blocks}"
 end
 
-def print_type(row, column, files)
-  name = files[column * number_of_rows(files) + row]
+def number_of_files(files)
+  files.length
+  end
+
+def number_of_rows(files)
+  (number_of_files(files).to_f / NUMBER_OF_COLUMNS).ceil
+end
+
+
+
+def print_type(name)
   type = File.ftype(name).to_s
   case type
   when type == 'file'
@@ -70,30 +66,27 @@ def print_type(row, column, files)
   end
 end
 
-def print_mode(row, column, files)
-  name = files[column * number_of_rows(files) + row]
+def print_mode(name)
   (-3).upto(-1) do |num|
     mode = File.stat(name).mode.to_s(8)[num]
-    print mode.gsub(/0|1|2|3|4|5|6|7/, '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx',\
-                                       '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx')
+    print mode.gsub(/[0-7]/, '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx',\
+                             '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx')
   end
 end
 
-def file_detail(row, column, files)
-  name = files[column * number_of_rows(files) + row]
+def print_file_detail(name)
   return unless name
 
-  print_type(row, column, files)
-  print_mode(row, column, files)
+  print_type(name)
+  print_mode(name)
   print File.stat(name).nlink.to_s.rjust(NLINK_WIDTH)
   print Etc.getpwuid(File.stat(name).gid).name.to_s.rjust(DETAIL_WIDTH)
   print Etc.getgrgid(File.stat(name).uid).name.to_s.rjust(DETAIL_WIDTH)
   print File.size(name).to_s.rjust(DETAIL_WIDTH)
-  print_timestamp(row, column, files)
+  print_timestamp(name)
 end
 
-def print_timestamp(row, column, files)
-  name = files[column * number_of_rows(files) + row]
+def print_timestamp(name)
   difference = days_difference(name)
   format = difference <= HALF_A_YEAR ? '%b %e %R ' : '%b %e  %Y '
   print File.mtime(name).strftime(format).to_s.rjust(TIMESTAMP_WIDTH)
