b6049082c1abfb3bf18b35ab721244a2715bc5a4
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-01-24 18:18:35 +0900

    fixed width method

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,10 +13,8 @@ def main
     interactive_mode(option)
   else
     # 出力物間のスペース数を、バイト数の合計値の桁数で調整する
-    total_bytes = total_bytes(targets, option)
-
-    calc_file(targets, option, total_bytes)
-    print_total(targets, option, total_bytes) if targets.size > 1
+    width = width(targets)
+    calc_file(targets, option, width)
   end
 end
 
@@ -34,54 +32,42 @@ def format_interactive(object)
   object.to_s.rjust(WIDTH)
 end
 
-def output(targets)
-  { lines: targets.count("\n"),
-    words: targets.split(/\s+/).size,
-    bytes: targets.size }
-end
-
-# total_bytesのみ再利用するためメソッドにした
-def total_bytes(targets, _option)
-  targets.map { |target| File.read(target).size }.sum
+def output(content)
+  { lines: content.count("\n"),
+    words: content.split(/\s+/).size,
+    bytes: content.size }
 end
 
 def calc_file(targets, option, width)
-  targets.each do |file_name|
-    targets = File.read(file_name)
-    print format_files(output(targets)[:lines], width)
-    unless option['l']
-      print format_files(output(targets)[:words], width)
-      print format_files(output(targets)[:bytes], width)
-    end
-    puts " #{file_name}"
-
-  end
-end
-
-# 利用1:文字数を計算して出力内容の間隔を指定する
-def width(total_bytes)
-  total_bytes.to_s.size + MARGIN
-end
-
-def print_total(targets, option, total_bytes)
   total_lines = 0
   total_words = 0
-  targets.each do |filename|
-    targets = File.read(filename)
-    total_lines += output(targets)[:lines]
-    total_words += output(targets)[:words]
+  total_bytes = 0
+  targets.each_with_index do |file_name, index|
+    content = File.read(file_name)
+    print format_files(output(content)[:lines], width)
+    print format_files(output(content)[:words], width) unless option['l']
+    print format_files(output(content)[:bytes], width) unless option['l']
+    puts " #{file_name}"
+
+    total_lines += output(content)[:lines]
+    total_words += output(content)[:words]
+    total_bytes += output(content)[:bytes]
+    # ファイルが複数あるとき、ループの最後で合計を出力する
+    next if index < targets.size - 1 || targets.size == 1
+
+    print format_files(total_lines, width)
+    print format_files(total_words, width) unless option['l']
+    print format_files(total_bytes, width) unless option['l']
+    puts ' total'
   end
-  # 利用2:total_bytes自体の出力
-  print format_files(total_lines, total_bytes)
-  unless option['l']
-    print format_files(total_words, total_bytes)
-    print format_files(total_bytes, total_bytes)
-  end
-  puts ' total'
 end
 
-def format_files(object, total_bytes)
-  width = width(total_bytes)
+# byte計から文字数を計算して出力内容の間隔を指定する
+def width(targets)
+  targets.map { |target| File.read(target).size }.sum.to_s.size + MARGIN
+end
+
+def format_files(object, width)
   object.to_s.rjust(width)
 end
 
