33ced54b916abc82f4dbe982fee3b8ec645aa8be
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-05-23 10:50:16 +0900

    removed code that makes instance variables local variables again

--- 08.ls_object/lib/short_style.rb
+++ 08.ls_object/lib/short_style.rb
@@ -9,12 +9,12 @@ class ShortStyle
   end
 
   def draw
-    files = @files
-    width = calc_width(files)
-    (0...number_of_rows(files)).each do |row|
+    # files = @files
+    width = calc_width
+    (0...number_of_rows).each do |row|
       (0...NUMBER_OF_COLUMNS).each do |column|
-        regulation = column * number_of_rows(files) + row
-        file = files[regulation]
+        regulation = column * number_of_rows + row
+        file = @files[regulation]
         show_content(file.path, width)
       end
       print "\n"
@@ -23,20 +23,20 @@ class ShortStyle
 
   private
 
-  def number_of_rows(files)
-    (number_of_files(files).to_f / NUMBER_OF_COLUMNS).ceil
+  def number_of_rows
+    (number_of_files.to_f / NUMBER_OF_COLUMNS).ceil
   end
 
-  def number_of_files(files)
-    files.length
+  def number_of_files
+    @files.length
   end
 
   def show_content(file, width)
     print file.ljust(width) if file
   end
 
-  def calc_width(files)
-    filepath = files.map(&:path)
+  def calc_width
+    filepath = @files.map(&:path)
     filepath.max_by(&:length).length + MARGIN
   end
 end
