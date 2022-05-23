e28724310be51523216404a799189541bf47649e
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-05-23 12:46:15 +0900

    Removed number of rows method

--- 08.ls_object/lib/short_style.rb
+++ 08.ls_object/lib/short_style.rb
@@ -10,11 +10,12 @@ class ShortStyle
 
   def draw
     width = calc_width
+    number_of_rows = (@files.length.to_f / NUMBER_OF_COLUMNS).ceil
     (0...number_of_rows).each do |row|
       (0...NUMBER_OF_COLUMNS).each do |column|
         regulation = column * number_of_rows + row
         file = @files[regulation]
-        show_content(file.path, width)
+        show_content(file.path, width) if file
       end
       print "\n"
     end
@@ -22,12 +23,8 @@ class ShortStyle
 
   private
 
-  def number_of_rows
-    (@files.length.to_f / NUMBER_OF_COLUMNS).ceil
-  end
-
   def show_content(file, width)
-    print file.ljust(width) if file
+    print file.ljust(width)
   end
 
   def calc_width
